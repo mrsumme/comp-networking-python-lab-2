06f71b66ace3df03ee71903c728d0983342fe3a6
mrsumme <mrsumme@clemson.edu>
2024-02-17 08:47:13 -0500

    Add Connection header to 200

--- webServer.py
+++ webServer.py
@@ -37,7 +37,7 @@ def webServer(port=13331):
       #Fill in start 
               
       #Content-Type is an example on how to send a header as bytes. There are more!
-      outputdata = b"HTTP/1.0 200 OK; Server: PythonLab2; Content-Type: text/html; charset=UTF-8\r\n\r\n"
+      outputdata = b"HTTP/1.0 200 OK; Server: PythonLab2; Connection: close; Content-Type: text/html; charset=UTF-8\r\n\r\n"
 
 
       #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
